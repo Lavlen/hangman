781071117e40bf4fb4ddcf2640194624db9c35cb
Lavlen <lavin.lennon@gmail.com>
2021-12-03 00:21:49 +0000

    add libraries and word file, add hangman image

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+from time import sleep
+
+#  Print a diagram of hangman
+print('============ HANGMAN ========')
+print('   ',  '______')
+print('   ',  '|    |')
+print('   ',  '|    O')
+print('   ',  '|   /|\\')
+print('   ',  '|   / \\')
+print()
+print('=============================')
+
+print('Thinking of a word')
