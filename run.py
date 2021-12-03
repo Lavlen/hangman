b01abac7613ec71db796e67cdfe4567b248a1031
Lavlen <lavin.lennon@gmail.com>
2021-12-03 00:51:03 +0000

    add functions for 3 seconds delay, to get random word and to start playing the game

--- run.py
+++ run.py
@@ -13,3 +13,36 @@ print()
 print('=============================')
 
 print('Thinking of a word')
+
+# Time delay function
+def delay():
+    for i in range(3):
+        print('.', end="")
+        sleep(.3)  # wait 3 seconds
+    print()
+delay()
+
+
+def get_word():
+    # randomly chooses a word from the list
+    word = random.choice(word_list)
+    return word.upper()  # make word uppercase
+
+def start_playing():
+    """
+    display random word as string of underscore
+    create variables for storing lists of
+    guessed letters, words and number of valid guesses
+    """
+    secret_word = "_" * len(word)
+    print("The word has", len(secret_word), "letters")
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    attempts = 6
+    print()
+    print('==============================')
+    print(body_parts(attempts))
+    print(secret_word)
+    print('')
+    print('')
