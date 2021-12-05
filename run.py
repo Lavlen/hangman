9f88d3dcd7bb09bea24a7512ddd395de5a6bbb1b
Lavlen <lavin.lennon@gmail.com>
2021-12-05 20:19:17 +0000

    amend print statements, amend if statement to check word input

--- run.py
+++ run.py
@@ -51,19 +51,17 @@ def start_playing(word):
     create variables for storing lists of
     guessed letters, words and number of valid guesses
     """
-    secret_word = "_" * len(word)
+    secret_word = "-" * len(word)
     print("The word has", len(secret_word), "letters")
     guessed = False
     guessed_letters = []
     guessed_words = []
     attempts = 6
     print()
-    print('==============================')
     print(body_parts(attempts)) # print hangman stages
     print(secret_word)  # print random placeholders
     print('')
-    print('')
-
+    
 
     # Check if word has been guessed and attempts are more than zero
     while not guessed and attempts > 0:
@@ -72,27 +70,27 @@ def start_playing(word):
         print(attempts, 'tries left, letters used ', ' '.join(guessed_letters))
 
         # receive user input and convert to uppercase
-        guess = input('please guess a letter: ').upper()
+        user_input = input('please guess a letter or a word: ').upper()
         """
         check if one letter has been inputted
         and if it has been tried before
         otherwise if the letter is not in the word say so
         """
-        if len(guess) == 1 and guess in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            if guess in guessed_letters:
-                print('Letter already guessed', guess)
-            elif guess not in word:
-                print(guess, 'is not in the word')
+        if len(guess) == 1 and user_input in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            if user_input in guessed_letters:
+                print(' Letter already tried', user_input)
+            elif user_input not in word:
+                print(user_input, 'is not in the word')
                 """
                 decrement from remaining attempts
                 add guessed letters to guessed_letters list
                 """
                 attempts -= 1
-                guessed_letters.append(guess)
-                print('incorrect letters already tried: ', guess)
+                guessed_letters.append(user_input)
+                print('incorrect letters already tried: ', user_input)
             else:
-                print('This letter,', guess, 'is in the word!')
-                guessed_letters.append(guess)
+                print('This letter,', user_input, 'is in the word!')
+                guessed_letters.append(user_input)
                 """
                 Convert from string to list for
                 indexing and store in new variable
@@ -102,18 +100,28 @@ def start_playing(word):
                 Find indices where guess is in the
                 word and the letter at each index
                 """
-                ind = [i for i, letter in enumerate(word) if letter == guess]
+                ind = [i for i, letter in enumerate(word)
+                       if letter == user_input]
+                # replace underscore with user_input
                 for index in ind:
-                    word_as_list[index] = guess
+                    word_as_list[index] = user_input
                 secret_word = "".join(word_as_list)
-                if '_' not in secret_word:
+                if '-' not in secret_word:
                     guessed = True
-        elif len(guess) > 1:
-            print('Guess a letter at a time')
-            attempts -= 1
+        elif len(user_input) == len(word) and user_input.isalpha():
+            if user_input in guessed_words:
+                print(user_input, 'you already tried that word')
+            elif user_input != word:
+                print(user_input, 'is not the word')
+                attempts -= 1
+                guessed_words.append(user_input)
+            else:
+                guessed = True
+                secret_word = word
+                print("=============================================")
 
         else:
-            print('Only letters are allowed')
+            print(user_input, 'is not a valid input')
         print(body_parts(attempts))
         print('==================================')
         print('')
@@ -123,10 +131,13 @@ def start_playing(word):
         print('Well done, you guessed the word!', word)
     else:
         print('you ran out of attempts. The word is: ', word)
+        userInput = input("Enter 'R' to restart or 'X' to exit: ")
 
 
 def body_parts(attempts):
-   
+    """
+    function that prints hangman body parts
+    """
     return stages[attempts]
 
 
